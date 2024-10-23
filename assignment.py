8a065612e804527c2a8cc484acd84a33a8f29b54
Kamto-Oguanobi <kamto.oguanobi@stu.ocsb.ca>
2024-10-23 14:34:57 +0000

    sync1

--- assignment.py
+++ assignment.py
@@ -1,1 +1,17 @@
+import math
 
+# input for diameter
+diameter = float(input("Enter the diameter of the circle: "))
+
+# for radius
+radius = diameter / 2
+
+# for area
+area = math.pi * radius ** 2
+
+# for circumference
+circumference = 2 * math.pi * radius
+
+# Print part
+print("The area of the circle is:", area)
+print("The circumference of the circle is:", circumference)
--- handler.php
+++ handler.php
@@ -1,23 +1,43 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
+<html lang="en">
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <meta charset="UTF-8">
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+  <link rel="manifest" href="favicon/site.webmanifest">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Area & Circumference Calculator</title>
+</head>
+
+<body>
+
+  <div cl<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Area & Circumference Calculator</span>
+        <div class="mdl-layout-spacer"></div>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="page-content" style="text-align: center; margin-top: 50px;">
+
+
+        <?php
+        if (isset($_POST['radius']) && is_numeric($_POST['radius'])) {
+          $radius = floatval($_POST['radius']);
+          $area = round(pi() * $radius * $radius, 2);
+          $circumference = round(2 * pi() * $radius, 2);
+          echo "<p>Area: $area square units</p>";
+          echo "<p>Circumference: $circumference units</p>";
+        }
+        ?>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
